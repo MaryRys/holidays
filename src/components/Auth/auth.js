0c2b02d87c8e41a2a2c6bccf2863ef7b3867be52
Mary Rys <rys.mary@gmail.com>
2018-11-13 20:52:51 -0600

    logout working

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -1,11 +1,15 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
+import googleImage from './sign-in-button.png';
 import './auth.scss';
 
 const loginButton = () => {
   const domString = `
-  <button id="google-auth" class="btn btn-secondary">login</button>
+  <button id="google-auth" class="btn btn-secondary">
+    <img src="${googleImage}" width="200px"/>
+  </button>
   `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
